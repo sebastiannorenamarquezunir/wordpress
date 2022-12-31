6f4d15207c07675d9f5e2c1ed9de3846041460d4
sebastian noreña marquez <sebastian.norena538@comunidadunir.net>
2022-12-31 09:05:07 -0500

    deploy wordpress

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -22,23 +22,12 @@ package 'curl'
 
 server_fqdn = node['fqdn']
 
-remote_file "#{Chef::Config[:file_cache_path]}/latest.tar.gz" do
-  source "http://wordpress.org/latest.tar.gz"
-  mode "0644"
-end
-
 directory "#{node['wordpress']['dir']}" do
   owner "root"
   group "root"
   mode "0755"
   action :create
   recursive true
-end
-
-execute "untar-wordpress" do
-   command "tar --strip-components 1 -xzf #{Chef::Config[:file_cache_path]}/latest.tar.gz"
-   cwd node['wordpress']['dir']
-   creates "#{node['wordpress']['dir']}/wp-settings.php"
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/wp-cli.phar" do
@@ -52,20 +41,23 @@ end
 
 file '/usr/local/bin/wp' do
    mode '777'
- end
-
- execute "cofig user" do
-   command ' cd #{node[\'wordpress\'][\'dir\']}  && sudo wp config create --dbname=#{node[\'wordpress\'][\'db\'][\'database\']} --dbuser=root --allow-root   && sudo  wp config set DB_NAME #{node[\'wordpress\'][\'db\'][\'database\']} --allow-root && sudo  wp config set DB_USER #{node[\'wordpress\'][\'db\'][\'user\']}  --allow-root && sudo  wp config set  DB_PASSWORD #{node[\'wordpress\'][\'db\'][\'password\']}   --allow-root && sudo  wp config set  DB_HOST -\'localhost\'  --allow-root && sudo  wp config set  FORCE_SSL_LOGIN true  --allow-root && sudo  wp config set  FORCE_SSL_ADMIN true  --allow-root && sudo  wp config set  CONCATENATE_SCRIPTS true  --allow-root &&  sudo  wp config set  SCRIPT_DEBUG true  --allow-root'
 end
 
-execute "wp-user-settings" do
-   command ' cd #{node[\'wordpress\'][\'dir\']}  && sudo  wp core install  --title=#{node[\'wordpress\'][\'title_wp\']} --admin_user=#{node[\'wordpress\'][\'user_wp\']} --admin_password=#{node[\'wordpress\'][\'db\'][\'password\']} --admin_email=#{node[\'wordpress\'][\'user_wp\']}@gmail.com  --url=http://=#{node[\'wordpress\'][\'url\']}/  --allow-root'
+execute "1" do
+   command "sudo mysql --user=root --password=#{node['wordpress']['db']['password']} --execute=\"create database IF NOT EXISTS #{node['wordpress']['db']['database']};CREATE USER IF NOT EXISTS #{node['wordpress']['db']['user']}@localhost;SET PASSWORD FOR #{node['wordpress']['db']['user']}@localhost = \'#{node['wordpress']['db']['password']}\';GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON #{node['wordpress']['db']['database']}.* TO #{node['wordpress']['db']['user']}@localhost;FLUSH PRIVILEGES;\""
 end
 
-execute "wp-theme-settings" do
-   command 'cd #{node[\'wordpress\'][\'dir\']}  && sudo  wp theme install wp-bootstrap-starter --activate  --allow-root'
-end  
 
-execute "wp-theme-create" do
-   command 'cd #{node[\'wordpress\'][\'dir\']}  &&  sudo  wp post create --post_status=\'publish\' --post_title=#{node[\'wordpress\'][\'name_post\']} --post_content=#{node[\'wordpress\'][\'name_post\']}  --allow-root'
+execute 'Execute a shell' do
+ command "bash -c \"cd #{node['wordpress']['dir']} && sudo wp core download --allow-root && sudo wp config create --dbname=#{node['wordpress']['db']['database']} --dbuser=root --allow-root && sudo  wp config set DB_NAME #{node['wordpress']['db']['database']} --allow-root && sudo  wp config set DB_USER #{node['wordpress']['db']['user']}  --allow-root && sudo wp config set  DB_PASSWORD #{node['wordpress']['db']['password']}  --allow-root && sudo wp config set  DB_HOST localhost  --allow-root && sudo  wp config set  FORCE_SSL_LOGIN true  --allow-root && sudo  wp config set  FORCE_SSL_ADMIN true  --allow-root && sudo  wp config set  CONCATENATE_SCRIPTS true  --allow-root  && sudo  wp config set  SCRIPT_DEBUG true  --allow-root \" "
+ not_if { File.exist?("/var/www/wordpress/wp-config.php") }
 end
+
+execute 'Execute create file' do
+   command "bash -c \"cd #{node['wordpress']['dir']} && sudo  wp core install --title=\'#{node['wordpress']['title_wp']}\' --admin_user=\'#{node['wordpress']['user_wp']}\' --admin_password=\'#{node['wordpress']['db']['password']}\' --admin_email=\'#{node['wordpress']['user_wp']}@gmail.com\'  --url=\'http://#{node['wordpress']['url']}\'  --allow-root \" "
+end
+
+execute 'theme' do
+   command "bash -c \"cd #{node['wordpress']['dir']} && sudo  wp theme install wp-bootstrap-starter --activate  --allow-root && sudo  wp post create --post_status='publish' --post_title\'node['wordpress']['name_post']}\' --post_content=\'#{node['wordpress']['name_post']}\'  --allow-root \" "
+end
+
--- wordpress/recipes/mysql.rb
+++ wordpress/recipes/mysql.rb
@@ -7,7 +7,3 @@
   package 'mysql-server' do
     action :install
   end
-
-  execute "Run_SQL_Files" do
-    command "sudo mysql --user=root --password=#{node['wordpress']['db']['password']} --execute=\"create database IF NOT EXISTS #{node['wordpress']['db']['database']};CREATE USER IF NOT EXISTS #{node['wordpress']['db']['user']}@localhost;SET PASSWORD FOR #{node['wordpress']['db']['user']}@localhost =\'#{node['wordpress']['db']['password']}\';FLUSH PRIVILEGES;\""
-  end
