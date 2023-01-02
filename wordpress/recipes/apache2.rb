4b41e62ceb382401a97c84b7d5f1c50c71086c40
Esteban Jimenez <yeisson310@hotmail.com>
2023-01-02 15:56:24 -0500

    Added 4 unit test for apache2 reipies

--- wordpress/recipes/apache2.rb
+++ wordpress/recipes/apache2.rb
@@ -4,33 +4,33 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 apt_update 'Update the apt cache daily' do
-    frequency 86400
-    action :periodic
-  end
-
-package "apache2" do
-    action :install
+  frequency 86400
+  action :periodic
 end
 
-service "apache2" do
-    action [:enable, :start]
+package 'apache2' do
+  action :install
 end
 
 service 'apache2' do
-    supports :status => true
-    action :nothing
+  action [:enable, :start]
+end
+
+service 'apache2' do
+  supports :status => true
+  action :nothing
 end
 
 file '/etc/apache2/sites-enabled/000-default.conf' do
-    action :delete
+  action :delete
 end
 
 template '/etc/apache2/sites-available/vagrant.conf' do
-    source 'virtual-hosts.conf.erb'
-    notifies :restart, resources(:service => "apache2")
+  source 'virtual-hosts.conf.erb'
+  notifies :restart, resources(:service => 'apache2')
 end
 
 link '/etc/apache2/sites-enabled/vagrant.conf' do
-    to '/etc/apache2/sites-available/vagrant.conf'
-    notifies :restart, resources(:service => "apache2")
+  to '/etc/apache2/sites-available/vagrant.conf'
+  notifies :restart, resources(:service => 'apache2')
 end
--- wordpress/spec/unit/recipes/apache2_spec.rb
+++ wordpress/spec/unit/recipes/apache2_spec.rb
@@ -26,4 +26,25 @@ describe 'wordpress::apache2' do
       expect { chef_run }.to_not raise_error
     end
   end
+
+  context 'Pakage apache2 is present' do
+    it 'install a package apache2' do
+      expect(chef_run).to install_package('apache2')
+    end
+  end
+
+  context 'Configuration files' do
+    it 'Delete 000-default.conf file' do
+      expect(chef_run).to delete_file('/etc/apache2/sites-enabled/000-default.conf')
+    end
+    it 'creates a template with the default action' do
+      expect(chef_run).to create_template('/etc/apache2/sites-available/vagrant.conf')
+    end
+  end
+
+  context 'Apache2 available' do
+    it 'Apache2 is running' do
+      expect(chef_run).to start_service('apache2')
+    end
+  end
 end
