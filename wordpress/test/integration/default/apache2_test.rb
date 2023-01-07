7667ef8348696d93b2ad5c0ad37d28850f45cc57
sebastian noreña marquez <sebastian.norena538@comunidadunir.net>
2023-01-07 02:10:26 -0500

    add test

--- wordpress/test/integration/default/apache2_test.rb
+++ wordpress/test/integration/default/apache2_test.rb
@@ -3,33 +3,34 @@
 # The Chef InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
 
+if os[:family] =='ubuntu'
+    describe package('apache2') do
+      it { should be_installed }
+    end
 
-describe package('apache2') do
-  it { should be_installed }
-end
+    describe file('/etc/apache2/sites-enabled/000-default.conf') do
+      it { should_not exist }
+    end
 
-describe file('/etc/apache2/sites-enabled/000-default.conf') do
-  it { should_not exist }
-end
+    describe file('/etc/apache2/sites-available/vagrant.conf') do
+      it { should exist }
+    end
 
-describe file('/etc/apache2/sites-available/vagrant.conf') do
-  it { should exist }
-end
+    describe service('apache2') do
+      it { should be_enabled }
+      it { should be_running }
+    end
 
-describe service('apache2') do
-  it { should be_enabled }
-  it { should be_running }
-end
+    describe port(80) do
+      it { should be_listening }
+      its('protocols') { should include 'tcp' }
+      its('addresses') { should include '0.0.0.0' }
+    end
 
-describe port(80) do
-  it { should be_listening }
-  its('protocols') { should include 'tcp' }
-  its('addresses') { should include '0.0.0.0' }
-end
-
-describe port(22) do
-  it { should be_listening }
-  its('processes') { should include 'sshd' }
-  its('protocols') { should include 'tcp' }
-  its('addresses') { should include '0.0.0.0' }
+    describe port(22) do
+      it { should be_listening }
+      its('processes') { should include 'sshd' }
+      its('protocols') { should include 'tcp' }
+      its('addresses') { should include '0.0.0.0' }
+    end
 end
